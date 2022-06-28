663555eaae80974c56aa69f817a32ee20ff94eec
kttss <kattouss.issam@gmail.com>
2022-06-28 09:24:54 +0100

    feat(api): post agence and load user list

--- apps/ges-cars-front/src/app/pages/new-agence/new-agence.component.ts
+++ apps/ges-cars-front/src/app/pages/new-agence/new-agence.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AgenceService } from '../../services/agence.service';
+import { AlertService } from '../../services/alert.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'ges-cars-new-agence',
   templateUrl: './new-agence.component.html',
   styleUrls: ['./new-agence.component.scss'],
 })
-export class NewAgenceComponent {
+export class NewAgenceComponent implements OnInit {
   agenceForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required]),
@@ -15,19 +18,32 @@ export class NewAgenceComponent {
     telephones: new FormControl([], [Validators.required]),
     emails: new FormControl([], [Validators.required]),
     faxs: new FormControl([], [Validators.required]),
-    admins: new FormControl([], [Validators.required]),
+    users: new FormControl([], [Validators.required]),
     logo: new FormControl('logo'),
   });
-  users = [
-    { value: '0', viewValue: 'issam' },
-    { value: '1', viewValue: 'test' },
-  ];
+  users = [];
 
   get form() {
     return this.agenceForm.controls;
   }
 
-  constructor(private agenceService: AgenceService) {}
+  constructor(
+    private agenceService: AgenceService,
+    private userService: UserService,
+    private alert: AlertService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.getAll().subscribe((res: any) => {
+      this.users = res.map((u: any) => {
+        return {
+          value: u.id,
+          viewValue: u.firstname + ' ' + u.lastname,
+        };
+      });
+    });
+  }
 
   onSubmit() {
     if (this.agenceForm.invalid) {
@@ -38,6 +54,8 @@ export class NewAgenceComponent {
         .create({ ...this.agenceForm.value })
         .subscribe((data) => {
           console.log(data);
+          this.alert.success('une agence a ete ajouté');
+          this.router.navigate(['/agence/list']);
         });
     }
   }
